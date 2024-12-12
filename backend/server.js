5f59069522ab9d72313494b5fcc79aa1e555dc59
Nouhaila Ben Aissa <nohailabenaissa789@gmail.com>
2024-12-12 19:37:49 +0100

    testing the server

--- backend/server.js
+++ backend/server.js
@@ -5,22 +5,30 @@ const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 
 
-// Initialize dotenv to load environment variables
+// Loads environment variables from a .env file into process.env.
 dotenv.config();
 
-// Initialize Express app
+// Creates an instance of an Express application
 const app = express();
 
-// Middleware
-app.use(bodyParser.json()); // To parse JSON bodies
+// Adds middleware to parse JSON bodies of incoming requests.
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    console.log('Request Body:', req.body);
+    next();
+});
 
 // Sample route for testing
 app.get('/', (req, res) => {
     res.send('Hello, World!');
-  });
-  
+});
+
   // Start the server
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-  });
+});
+
+
+  // how does this shishi work
