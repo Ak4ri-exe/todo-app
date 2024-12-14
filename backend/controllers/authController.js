1a9c24d16fd80fc50bb0cdf6a31e1512ebb48ee4
Nouhaila Ben Aissa <nohailabenaissa789@gmail.com>
2024-12-14 13:50:02 +0100

    hashed the password and stored it in DB , now am trying to compare passwords for login

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,16 +1,17 @@
-const bcrypt = require('bcrypt');
 const sql = require('../services/supabaseService');
-
+const bcrypt = require('bcrypt');
 
 exports.register = async (req, res) => {
     const { username, email, password } = req.body;
-
+    // hashing the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log('Hashed password:', hashedPassword );
     try {
         const result = await sql`
         INSERT INTO users
         (username, email, password) 
         VALUES
-        (${username}, ${email}, ${password}) 
+        (${username}, ${email}, ${hashedPassword}) 
         RETURNING *`; 
         res.status(201).json(result[0]);
     } catch (err){
@@ -26,7 +27,17 @@ exports.login = async (req, res) => {
         WHERE username = ${username} AND password = ${password}`
         // check if user with those creentials exits in the DB
         if (result.length > 0){
-            res.status(200).json({message: 'user logged in successfully'});
+            const user = result[0];
+            console.log('pw 1',user.password);
+            console.log('pw 2',password);
+
+            const match = await bcrypt.compare(password, user.password);
+            if (match) {
+                res.status(200).json({message: 'user logged in successfully'});
+            }
+            else{
+                res.status(401).json({message: 'Invalid Username or Password'})
+            }
         }
         else{
         res.status(401).json({message: 'Invalid Username or Password'})
