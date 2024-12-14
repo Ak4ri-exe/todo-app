c25b9aa696b749785ce9d9cf0e103f8a13074f8b
Nouhaila Ben Aissa <nohailabenaissa789@gmail.com>
2024-12-14 12:44:33 +0100

    connected directly to postgres database of supabase instead of using api to connect to supabase + added routes for login /register and user httpie app to send requests and recieve responds of my server

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,14 @@
-// Import required packages
-
-const express = require('express'); // require() -> include modules
+const express = require('express'); 
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+const postgres = require('postgres');
 
-
-// Loads environment variables from a .env file into process.env.
 dotenv.config();
 
-// Creates an instance of an Express application
+
 const app = express();
 
-// Middleware functions are functions that process incoming requests before they reach the routes
-// Adds middleware to parse JSON bodies of incoming requests.
+
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
@@ -20,22 +16,62 @@ app.use((req, res, next) => {
     next();
 });
 
+const sql = postgres({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  ssl: { rejectUnauthorized: false }
+});
 
 // testing routes -> send response for each route
 app.get('/', (req, res) => {
     res.send('server test test');
 });
 
-app.get('/about', (req, res) => {
-  res.send('about page');
+app.post('/register', async (req, res) => {
+  const { username, email, password } = req.body;
+
+    try {
+      const result = await sql`
+      INSERT INTO users
+      (username, email, password) 
+      VALUES
+      (${username}, ${email}, ${password}) 
+      RETURNING *`; 
+      res.status(201).json(result[0]);
+    } catch (err){
+      res.status(500).json({error: err.message});
+    }
 });
 
-app.get('/contact', (req, res) => {
-  res.send('contact page');
+app.post('/login', async (req, res) => {
+  const {username, password} = req.body;
+  try {
+    const result = await sql`
+    SELECT username, password from users
+    WHERE username = ${username} AND password = ${password}`
+    // check if user with those creentials exits in the DB
+    if (result.length > 0){
+      res.status(200).json({message: 'user logged in successfully'});
+    }
+    else{
+      res.status(401).json({message: 'Invalid Username or Password'})
+    }
+  }
+  catch {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.post('/lol', (req, res) => {
+
+    return res.status(500).json({message: "Errrrrrror"})
 });
 
   // Start the server and listen to incoming requests on this port
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.DB_PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
